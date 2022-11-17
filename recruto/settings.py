6ce4b8bc5521efcc6d404e6b66bef167d80f75b2
Lcoreen <lcoreen@student.21-school.ru>
2022-11-17 13:47:32 +0300

    Add hosts to settings

--- recruto/settings.py
+++ recruto/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fe)y5ck%-^w!c_q1xpj8p
 # DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['enigmatic-sea-41444.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
